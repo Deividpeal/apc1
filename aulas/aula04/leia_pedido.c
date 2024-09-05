3bb858f655dffb89cdd0ffe7abd1101f1554c2af
Deividpeal <deividpeal@gmail.com>
2024-09-05 04:25:21 +0000

    refiz

--- aulas/aula04/leia_pedido.c
+++ aulas/aula04/leia_pedido.c
@@ -4,13 +4,13 @@ int main () {
   char produto[21];
   int quantidade = 0;
   float valor = 0.0f;
-  printf("Digite o produto: ", produto);
+  printf("Digite o produto: ");
   scanf("%[^\n]s", produto);
-  printf("Digite a quantidade: ", quantidade);
+  printf("Digite a quantidade: ");
   scanf("%i", &quantidade);
-  print("Digite o valor: ", valor);
+  printf("Digite o valor: ");
+
   scanf("%f", &valor);
-  
 
   printf("==============================\n");
   printf("         P E D I D O\n");
