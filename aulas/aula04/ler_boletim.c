84e2f1f07df16dffa75bb6d6a92327a289c5ed9e
Deividpeal <deividpeal@gmail.com>
2024-09-11 22:01:50 +0000

    novo conteúdo

--- aulas/aula04/ler_boletim.c
+++ aulas/aula04/ler_boletim.c
@@ -3,7 +3,7 @@ int main() {
   int matricula = 0;
   float a1 = 0.0f;
   float a2 = 0.0f;
-   printf("Digite a matricula: ", matricula);
+  printf("Digite a matricula: ", matricula);
   scanf("%i", &matricula);
   printf("Digite a nota ", a1);  
   scanf("%f", &a1);
---
+++ aulas/aula05/incremento_decremento.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main () {
+
+  int numero = 0;
+  int incremento = numero;
+  int decremento = numero;
+  scanf("digite um numero %i\n", numero);
+    printf("O pré-incremento do número %i é %i\n", numero, ++incremento);
+    printf("O pré-decremento do número %i é %i\n", numero, --decremento);
+    printf("O pós-incremento do número %i é %i\n", numero, incremento++);
+    printf("O pós-decremento do número %i é %i\n", numero, decremento--);
+  
+
+
+
+  
+
+
+
+
+  return 0;
+}
