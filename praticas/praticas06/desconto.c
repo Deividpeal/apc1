2fd12b21aca83b9fb3c78d0f66ab592c44a6b2bf
Deivid Pereira Almeida <deividpeal@gmail.com>
2024-10-16 21:38:10 -0300

    aula nova

---
+++ praticas/praticas06/desconto.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main () {
+float valor_bruto;
+float valor_desconto;
+Printf("Digite o valor bruto e valor desconto: ", valor_bruto,valor_desconto);
+scanf("%f" &valor_bruto);
+
+if (valor_bruto <= 100.00f){
+   ( valor_desconto = valor_bruto * 0.01f)
+}else{ (valor_bruto >= 100.00f)
+     (valor_bruto >= 500.00f)
+}
+if (valor_desconto = valor_bruto * 0.05f){
+}else{ (valor_bruto <= 100.00f)
+    (valor_desconto = valor_bruto * 0.1f)
+}
+
+    return 0;
+}
